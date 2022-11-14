c8271b6c97582bc7a980d728e9fa93b69aecf98e
GraberThomas <thomas.graber310@gmail.com>
2022-11-14 20:30:45 +0100

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam maxime in ab reiciendis optio voluptates, adipisci magni. Iure minima fugit consequatur dolorum nisi a inventore aliquid impedit fugiat, temporibus libero, dolores eum quaerat ea esse laudantium delectus praesentium aut nostrum sint, est cumque qui numquam? Cum quas eveniet nam. Tempora.</p>
+    </div>
+  )
+}
+
+export default App
